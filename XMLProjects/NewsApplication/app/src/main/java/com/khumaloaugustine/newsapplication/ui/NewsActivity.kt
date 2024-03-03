22c29c4167316f82cbdc554afe82533f07c5cd6e
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-03 15:12:53 +0200

    Added Fragments

---
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/ui/NewsActivity.kt
@@ -0,0 +1,12 @@
+package com.khumaloaugustine.newsapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.khumaloaugustine.newsapplication.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
