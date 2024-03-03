359763a23e48185c5032939513f971deda387e0f
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-04 00:17:51 +0200

    Added ArticleFragment, HeadlinesFragment

--- XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/util/Constants.kt
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/util/Constants.kt
@@ -1,10 +1,10 @@
 package com.khumaloaugustine.newsapplication.util
 
 class Constants {
-    companion object{
+    companion object {
         const val API_KEY = "de5c61f80fe4477fbe7fb3faaf55308d"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "5"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
