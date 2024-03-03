3c94227a509670daec8a2813b025879be0fd71c2
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-03 18:40:26 +0200

    Added api

---
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/api/NewsAPI.kt
@@ -0,0 +1,29 @@
+package com.khumaloaugustine.newsapplication.api
+
+import com.khumaloaugustine.newsapplication.models.NewsResponse
+import com.khumaloaugustine.newsapplication.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/top-headlines")
+    suspend fun fetchHeadLines(
+        @Query("country")
+        countryCode: String = "sa",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+}
