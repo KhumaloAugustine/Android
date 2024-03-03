9ecbe83ce502738fc817a02568ac1fe90a071646
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-03 22:50:02 +0200

    Added ArticleFragment

--- XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/ui/fragments/ArticleFragment.kt
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/ui/fragments/ArticleFragment.kt
@@ -5,17 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.khumaloaugustine.newsapplication.R
-class ArticleFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+import com.khumaloaugustine.newsapplication.databinding.FragmentArticleBinding
+import com.khumaloaugustine.newsapplication.ui.NewsActivity
+import com.khumaloaugustine.newsapplication.ui.NewsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+    lateinit var newsViewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        val article = args.article
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let {
+                loadUrl(it)
+            }
+        }
+        binding.fab.setOnClickListener {
+            newsViewModel.addToFavourites(article)
+            Snackbar.make(view, "Added to favourites", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
