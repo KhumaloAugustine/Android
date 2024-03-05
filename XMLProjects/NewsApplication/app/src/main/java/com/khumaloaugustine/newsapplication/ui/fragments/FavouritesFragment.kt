cd3e4ba74adeeec24787a692e5da7e457375daf2
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-05 11:20:05 +0200

    resolved warning from FavouritesFragment

--- XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/ui/fragments/FavouritesFragment.kt
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/ui/fragments/FavouritesFragment.kt
@@ -1,12 +1,8 @@
 package com.khumaloaugustine.newsapplication.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,7 +17,7 @@ import com.khumaloaugustine.newsapplication.ui.NewsViewModel
 class FavouritesFragment : Fragment(R.layout.fragment_favourites) {
     lateinit var newsViewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
-    lateinit var binding: FragmentFavouritesBinding
+    private lateinit var binding: FragmentFavouritesBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -64,8 +60,8 @@ class FavouritesFragment : Fragment(R.layout.fragment_favourites) {
             attachToRecyclerView(binding.recyclerFavourites)
         }
         newsViewModel.getFavouriteNews().observe(
-            viewLifecycleOwner,
-            Observer { articles -> newsAdapter.differ.submitList(articles) })
+            viewLifecycleOwner
+        ) { articles -> newsAdapter.differ.submitList(articles) }
     }
 
     private fun setupFavouritesRecycler() {
