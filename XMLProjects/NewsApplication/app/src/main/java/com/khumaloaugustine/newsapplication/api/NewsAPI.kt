48a2b6ddc816503ecd888aa188fbac06d11fe5f2
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-06 16:47:20 +0200

    changed news api and NewsRepository

--- XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/api/NewsAPI.kt
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/api/NewsAPI.kt
@@ -9,21 +9,15 @@ import retrofit2.http.Query
 interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getHeadLines(
-        @Query("country")
-        countryCode: String = "us",
-        @Query("page")
-        pageNumber: Int = 1,
-        @Query("apiKey")
-        apiKey: String = API_KEY
-    ): Response<NewsResponse>
+        @Query("country") countryCode: String? = "us",
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String? = API_KEY
+    ): Response<NewsResponse?>
 
     @GET("v2/everything")
     suspend fun searchForNews(
-        @Query("q")
-        searchQuery: String,
-        @Query("page")
-        pageNumber: Int = 1,
-        @Query("apiKey")
-        apiKey: String = API_KEY
-    ): Response<NewsResponse>
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String? = API_KEY
+    ): Response<NewsResponse?>
 }
--- XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/repository/NewsRepository.kt
+++ XMLProjects/NewsApplication/app/src/main/java/com/khumaloaugustine/newsapplication/repository/NewsRepository.kt
@@ -3,12 +3,14 @@ package com.khumaloaugustine.newsapplication.repository
 import com.khumaloaugustine.newsapplication.api.RetrofitInstance
 import com.khumaloaugustine.newsapplication.database.ArticleDatabase
 import com.khumaloaugustine.newsapplication.models.Article
+import com.khumaloaugustine.newsapplication.models.NewsResponse
+import retrofit2.Response
 
 class NewsRepository(private val database: ArticleDatabase) {
-    suspend fun getHeadlines(countryCode: String, pageNumber: Int) =
+    suspend fun getHeadlines(countryCode: String?, pageNumber: Int): Response<NewsResponse?> =
         RetrofitInstance.api.getHeadLines(countryCode, pageNumber)
 
-    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+    suspend fun searchNews(searchQuery: String, pageNumber: Int): Response<NewsResponse?> =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
 
     suspend fun upsert(article: Article) = database.getArticleDao().upsert(article)
